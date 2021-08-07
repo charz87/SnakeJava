1fe7ea693c2c878a806f658eef5de09e9095094e
Charly Chavez <charly_chavper@hotmail.com>
2021-08-07 00:22:18 -0500

    Add files via upload

---
+++ src/SnakeGame.java
@@ -0,0 +1,11 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new GameFrame(); //Shortcut to create instance of a class, if is not going to be used for more than one task
+
+	}
+
+}
